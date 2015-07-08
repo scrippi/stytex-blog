96c90dbf6c650fa1cabe21a0683db5267dd6b44a
David Steiman <adinatbust@gmail.com>
2015-07-08 12:39:53 +0200

    more asset shit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+#Rails.application.config.assets.precompile += ['admin.js']
 # Precompile additional assets.
 # application.js, application.css.less, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
